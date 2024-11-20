240909fe67e24dc3c711e33030b7c6efe5953352
Alyssa Kucharyski <adkucharyski@gmail.com>
2024-11-20 15:49:13 -0700

    name

--- website/pages/api/companies/index.js
+++ website/pages/api/companies/index.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   try {
     // Fetch the list of companies from the database using the query function
     console.log('trying to fetch companies');
-    const companies = await query('SELECT * FROM companies WHERE company_name = $1', [company_name]); // SQL query to fetch companies
+    const companies = await query('SELECT * FROM companies'); // SQL query to fetch companies
     res.status(200).json(companies); // Return the companies as JSON
   } catch (error) {
     console.log(error);
